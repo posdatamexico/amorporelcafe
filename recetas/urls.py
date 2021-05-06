743d8faf58a91ddaf5a85f1d22ca5afe70270d52
ivonne cordero <posdatamexico@gmail.com>
2021-05-05 18:37:25 -0700

    template detalle

--- recetas/urls.py
+++ recetas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, publicar, nosotros
+from .views import home, publicar, nosotros, buscar, detail
 
 urlpatterns = [
 	path('', home.as_view(), name='home'),
 	path('publicar', publicar.as_view(), name='publicar'),
 	path('nosotros', nosotros, name='nosotros'),
+	path('buscar', buscar, name='buscar'),
+	path('detail', detail.as_view(), name='detail'),
 ]
--- recetas/views.py
+++ recetas/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 from .models import Recetas
 from .forms import RecetaForm
 
@@ -16,3 +16,20 @@ class publicar(CreateView):
 
 def nosotros(request):
 	return render(request, 'about.html')
+
+class detail(DetailView):
+	model = Recetas
+	template_name = 'detalle.html'
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(Detail, self).get_context_data(*args, **kwargs)
+		stuff = get_object_or_404(Receta, id=self.kwargs['pk'])
+		return context
+
+def buscar(request):
+	if request.method == "POST":
+		searched = request.POST['searched']
+		recetas = Recetas.objects.filter(nombre__contains=searched)
+		return render(request, 'recetas.html', {'searched':searched, 'recetas':recetas})
+	else:
+		return render(request, 'recetas.html')
